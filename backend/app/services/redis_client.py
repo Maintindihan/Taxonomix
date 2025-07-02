26a2e1fa2939a0f50fb363b94d3976e56480cddf
Maintindihan <Nainoaian@gmail.com>
2025-07-01 21:05:43 -1000

    Making some site launch additions.

--- backend/app/services/redis_client.py
+++ backend/app/services/redis_client.py
@@ -1,3 +1,5 @@
 import redis
+import os
 
-redis_client = redis.Redis(host="localhost", port=6379, decode_responses=True)
+# redis_client = redis.Redis(host="localhost", port=6379, decode_responses=True)
+redis_client = redis.Redis.from_url(os.getenv("REDIS_URL"))
