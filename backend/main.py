03fa4eaaa507e879259f190fd0d5b4b8c455bf22
Maintindihan <Nainoaian@gmail.com>
2025-07-03 13:18:01 -1000

    Made backend ready for production.

--- backend/main.py
+++ backend/main.py
@@ -4,9 +4,16 @@ from app.api import router as api_router
 
 app = FastAPI(title="Taxonomix API")
 
+origins = [
+    "https://taxonomix.net",
+    "https://www.taxonomix.net",
+    "https://api.taxonomix.net",
+]
+
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://www.taxonomix.net"],  # or ["*"] during testing
+    allow_origins=origins,  # or ["*"] during testing
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
