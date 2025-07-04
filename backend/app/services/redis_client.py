8f22e6adb2736948b40761ada0e5b37f2f3b5ffb
Maintindihan <Nainoaian@gmail.com>
2025-07-04 12:40:40 -1000

    Added a try/catch to my redis client

--- backend/app/services/redis_client.py
+++ backend/app/services/redis_client.py
@@ -2,4 +2,8 @@ import redis
 import os
 
 # redis_client = redis.Redis(host="localhost", port=6379, decode_responses=True)
-redis_client = redis.Redis.from_url(os.getenv("REDIS_URL"))
+
+try:
+    redis_client = redis.Redis.from_url(os.getenv("REDIS_URL"))
+except Exception as e:
+    print("Redis error:", e)
